0c55cdbc0cdd45c1c1a9f9d89292dcc9d995e1d4
Lucas <lucasmaximo@outlook.com>
2024-06-27 17:36:12 -0300

    Inserindo as validações

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 //array que vai receber os nomes dos amigos
-amigos = [];
-
-function adicionar(){
-    //adicionando os amigos na lista de amigos e limpa o campo depois de adicionar
-    let campoNomeAmigo = document.getElementById('nome-amigo');
-    amigos.push(campoNomeAmigo.value);
-    campoNomeAmigo.value = '';
-    
-    //listar os amigos adicionados no amigo secreto
-    listarAmigos(amigos);
-}
+let amigos = [];
+let sorteios = [];
 
 //função que imprime a lista de amigos no espaço de amigos incluídos
 function listarAmigos(lista){
     let listaAmigos = document.getElementById('lista-amigos');
     listaAmigos.textContent = lista.join(', ');
+}
+
+//adicionando os amigos na lista de amigos e limpa o campo depois de adicionar
+function adicionar(){
+    let campoNomeAmigo = document.getElementById('nome-amigo');
+
+    //validações
+    if(amigos.includes(campoNomeAmigo.value)){
+        alert('O nome já foi incluído. Preencha um nome diferente.');
+        campoNomeAmigo.value = '';
+        return;
+    }
+    if(campoNomeAmigo.value == ''){
+        alert('O nome não pode ser vazio. Por favor, preencha um nome.');
+        return;
+    }
+
+    amigos.push(campoNomeAmigo.value);
+    campoNomeAmigo.value = '';
+    
+    //listar os amigos adicionados no amigo secreto
+    listarAmigos(amigos);
 }
 
 //sorteia o amigo secreto
@@ -34,7 +47,12 @@ function sortearAmigo(amigo, amigosSorteados){
 
 //função que sorteia os amigos
 function sortear(){
-    let sorteios = [];
+    //validação
+    if(amigos.length < 4){
+        alert('O sorteio não pode ser feito com menos de 4 amigos. Por favor, insira mais amigos.');
+        return;
+    }
+
     //criando uma cópia do array original
     let amigosSorteados = amigos.slice();
 
@@ -49,8 +67,10 @@ function sortear(){
         //a lista de amigosSorteados é recriada excluindo o amigo que acabou de ser sorteado
         amigosSorteados = amigosSorteados.filter(a => a !== amigoSorteado);
     }
+    
     console.log(sorteios);
 
+    //imprime em tela os sorteios na lista de sorteios
     let listaSorteios = document.getElementById('lista-sorteio');
 
     sorteios.forEach(function(sorteio) {
@@ -62,6 +82,9 @@ function sortear(){
 
 //reinicia o sorteio do amigo secreto, apagando os amigos incluídos e o resultado do sorteio
 function reiniciar(){
-    document.getElementById('lista-amigos').innerHTML = '';
+    amigos = [];
+    document.getElementById('lista-amigos').textContent = '';
+
+    sorteios = [];
     document.getElementById('lista-sorteio').innerHTML = '';
 }
